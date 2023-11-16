e997bd1db5dfbf69b11759379c3931b42016df4e
SangDoo Nam <kexe009@hotmail.com>
2023-11-16 23:10:35 +0100

    finish

--- src/cat_controller.py
+++ src/cat_controller.py
@@ -1,4 +1,64 @@
+
+
 class CatController:
-    pass
     # TODO: You need to write the code for this class based on what the tests
     # expect.
+    def hourly_run(self, hour):  
+        
+        water = "Give water."
+        cage_open = "Open cat cage."
+        cage_close = "Close cat cage."
+        feed = "Feed"
+        food = ["1 mouse", "1 hamster", "1 chicken"]
+        result=[]
+        
+        if hour in [8, 12, 17]:
+            result += [feed]
+            if hour == 8:
+                result += [food[0]]
+            elif hour == 12:
+                result += [food[1]]
+            elif hour == 17:
+                result += [food[2]]
+        if hour in [8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19]:
+            result += [water]
+        elif hour == 7:
+            result += [cage_open]
+        elif hour == 20:
+            result += [cage_close]
+        else:
+            if hour == 12:
+                result
+            else:
+                result = []
+            
+        return result
+        
+
+
+
+# if hour corresponds to the each of 8,9,10,11,13,14,15,16,17,18 ans 19
+#    and  'Give water' is not bond with those numbers
+#       denn it is false
+# but 'Give water' is bond with other number than 8,9,10,11,13,14,15,16,17,18 ans 19
+#       denn it is also false
+
+# test_failed = False
+# cat_controller = CatController()
+
+# for hour in range(0, 23): 
+#     if hour in WATER_HOURS: 
+#         if not "Give water." in cat_controller.hourly_run(hour):
+#             test_failed = True
+#     elif "Give water." in cat_controller.hourly_run(hour):
+#         test_failed = True
+    
+#     print(hour, test_failed, cat_controller.hourly_run(hour))
+
+
+
+# not_yet_fed = ["1 mouse", "1 hamster", "1 chicken"]
+# not_yet_fed.remove("1 chicken")
+# print(not_yet_fed)
+
+
--- src/test.py
+++ src/test.py
@@ -10,8 +10,8 @@ def main():
     cat_controller = CatController()
 
     # Test that water is only given each hour 8-11 and 13-19.
-    for hour in range(0, 23):
-        if hour in WATER_HOURS:
+    for hour in range(0, 23): # 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
+        if hour in WATER_HOURS: # 8,9,10,11,13,14,15,16,17,18,19
             if not "Give water." in cat_controller.hourly_run(hour):
                 test_failed = True
         elif "Give water." in cat_controller.hourly_run(hour):
