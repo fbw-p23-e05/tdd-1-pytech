12c7baf4c19ec975ce6f250dfe37ff8a864c9e99
Ghost09a1 <139347108+Ghost09a1@users.noreply.github.com>
2023-11-21 09:27:00 +0100

    yoink

--- src/cat_controller.py
+++ src/cat_controller.py
@@ -1,4 +1,23 @@
+# src/cat_controller.py
+
 class CatController:
-    pass
-    # TODO: You need to write the code for this class based on what the tests
-    # expect.
+    def __init__(self):
+        self.cage_locked = False
+        self.meat_dispenser_status = "Idle"
+        self.water_dispenser_status = "Idle"
+
+    def lock_cage(self):
+        self.cage_locked = True
+        return "Cage locked."
+
+    def unlock_cage(self):
+        self.cage_locked = False
+        return "Cage unlocked."
+
+    def dispense_meat(self):
+        self.meat_dispenser_status = "Dispensing meat."
+        return "Meat dispensed."
+
+    def dispense_water(self):
+        self.water_dispenser_status = "Dispensing water."
+        return "Water dispensed."
