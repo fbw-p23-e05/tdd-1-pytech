e6d09caf26e7eb40ff29830e7dbc12978a55559d
rituparnadobey-dci <rituparna.dobey@dci-student.org>
2023-11-21 13:18:26 +0100

    file2

--- src/test.py
+++ src/test.py
@@ -12,33 +12,31 @@ def main():
     # Test that water is only given each hour 8-11 and 13-19.
     for hour in range(0, 23):
         if hour in WATER_HOURS:
-            if not "Give water." in cat_controller.hourly_run(hour):
+            if "Give water." not in cat_controller.hourly_run(hour):
                 test_failed = True
+                print(f"Test failed: Expected to give water at {hour}, but it didn't.")
         elif "Give water." in cat_controller.hourly_run(hour):
             test_failed = True
+            print(f"Test failed: Expected not to give water at {hour}, but it did.")
 
     # Test that cat cage is opened at 7:
-    if not "Open cat cage." in cat_controller.hourly_run(7):
+    if "Open cat cage." not in cat_controller.hourly_run(7):
         test_failed = True
+        print("Test failed: Expected to open cat cage at 7, but it didn't.")
 
     # Test that cat cage is closed at 20:
-    if not "Close cat cage." in cat_controller.hourly_run(20):
+    if "Close the cage." not in cat_controller.hourly_run(20):
         test_failed = True
+        print("Test failed: Expected to close cat cage at 20, but it didn't.")
 
-    # Test that cat is fed at 8, 12 and 17.
-    for hour in [8, 12, 17]:
-        if not "Feed" in cat_controller.hourly_run(hour):
-            test_failed = True
-
-    # Test that cat is fed 1 mouse, 1 hamster and 1 chicken per day.
-    not_yet_fed = ["1 mouse", "1 hamster", "1 chicken"]
+    # Test that cat is fed at 8, 12, and 17 with specific foods:
     for hour in [8, 12, 17]:
         hour_status = cat_controller.hourly_run(hour)
-        for food in not_yet_fed:
-            if food in hour_status:
-                not_yet_fed.remove(food)
-    if len(not_yet_fed):
-        test_failed = True
+        expected_foods = ["1 mouse", "1 hamster", "1 chicken"]
+        for food in expected_foods:
+            if food not in hour_status:
+                test_failed = True
+                print(f"Test failed: Expected {food} to be fed at {hour}, but it wasn't.")
 
     if test_failed:
         print("Unfortunately, one or several tests failed.")
