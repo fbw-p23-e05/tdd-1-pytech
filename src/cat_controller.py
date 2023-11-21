f6e18b2b31e361ae4449a97ff62a52e3d25a900c
Dci-student <sena.adigbo@dci-student.org>
2023-11-21 07:58:58 +0100

    solved

--- src/cat_controller.py
+++ src/cat_controller.py
@@ -1,4 +1,21 @@
 class CatController:
-    pass
-    # TODO: You need to write the code for this class based on what the tests
-    # expect.
+    def __init__(self) -> None:
+        self.additional_water_hours = [9, 10, 11, 13, 14, 15, 16, 18, 19]
+
+    def hourly_run(self, hour):
+        actions = set()
+
+        if hour in [8, 17]:
+            actions.update(['Give water.', "Feed", "1 mouse", "1 chicken"])
+        elif hour == 12:
+            actions.update(["Feed", "1 hamster"])
+        elif hour == 7:
+            actions.add("Open cat cage.")
+        elif hour == 20:
+            actions.add("Close cat cage.")
+        elif hour in self.additional_water_hours:
+            actions.add('Give water.')
+        else:
+            actions.add('Nothing')
+
+        return actions
