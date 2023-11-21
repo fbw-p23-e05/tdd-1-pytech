efb257886014d5fded2fb17db38554c8f9bc46c8
rituparnadobey-dci <rituparna.dobey@dci-student.org>
2023-11-21 13:16:20 +0100

    file1

--- src/cat_controller.py
+++ src/cat_controller.py
@@ -1,4 +1,25 @@
 class CatController:
-    pass
-    # TODO: You need to write the code for this class based on what the tests
-    # expect.
+    def hourly_run(self, hour):
+        self.hour = hour
+
+        if self.hour == 7:
+            return "Open cat cage."
+
+        elif self.hour == 8:
+            return "Give water. Feed 1 mouse."
+
+        elif self.hour in [9, 10, 11, 13, 14, 15, 16]:
+            return "Give water."
+
+        elif self.hour == 12:
+            return "Feed 1 hamster."
+
+        elif self.hour == 17:
+            return "Give water. Feed 1 chicken."
+
+        elif self.hour == 20:
+            return "Close the cage."
+        else:
+            return " "
+
+    
